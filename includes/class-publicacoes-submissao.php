391b6f98e636de8fc843c8c4faeb0722ea618f6e
Marcos Cordeiro <marcosc974@gmail.com>
2026-04-20 11:05:30 -0300

    :wrench: feat: alteração da legenda do menu

--- includes/class-publicacoes-submissao.php
+++ includes/class-publicacoes-submissao.php
@@ -1,62 +1,74 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (! defined('ABSPATH')) {
     exit;
 }
 
 require_once PUBLICACOES_SUBMISSAO_PATH . 'includes/admin.php';
 require_once PUBLICACOES_SUBMISSAO_PATH . 'includes/frontend.php';
 
-class Publicacoes_Submissao {
+class Publicacoes_Submissao
+{
     private static $instance = null;
     private $admin;
     private $frontend;
 
-    public static function instance() {
-        if ( null === self::$instance ) {
+    public static function instance()
+    {
+        if (null === self::$instance) {
             self::$instance = new self();
         }
 
         return self::$instance;
     }
 
-    private function __construct() {
+    private function __construct()
+    {
         $this->admin = new Publicacoes_Submissao_Admin();
         $this->frontend = new Publicacoes_Submissao_Frontend();
 
-        add_action( 'init', array( $this, 'register_cpt' ) );
-        add_action( 'init', array( $this->frontend, 'register_shortcodes' ) );
-        add_action( 'wp_enqueue_scripts', array( $this->frontend, 'register_assets' ) );
-        add_action( 'wp_ajax_publicacoes_like', array( $this->frontend, 'ajax_like' ) );
-        add_action( 'wp_ajax_nopriv_publicacoes_like', array( $this->frontend, 'ajax_like' ) );
-        add_action( 'wp_ajax_publicacoes_comment', array( $this->frontend, 'ajax_comment' ) );
-        add_action( 'wp_ajax_nopriv_publicacoes_comment', array( $this->frontend, 'ajax_comment' ) );
+        add_action('init', array($this, 'register_cpt'));
+        add_action('init', array($this->frontend, 'register_shortcodes'));
+        add_action('wp_enqueue_scripts', array($this->frontend, 'register_assets'));
+        add_action('wp_ajax_publicacoes_like', array($this->frontend, 'ajax_like'));
+        add_action('wp_ajax_nopriv_publicacoes_like', array($this->frontend, 'ajax_like'));
+        add_action('wp_ajax_publicacoes_comment', array($this->frontend, 'ajax_comment'));
+        add_action('wp_ajax_nopriv_publicacoes_comment', array($this->frontend, 'ajax_comment'));
+        wp_enqueue_style(
+            'publicacoes-submissao',
+            'https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap',
+            [],
+            null
+        );
     }
 
-    public static function activate() {
+    public static function activate()
+    {
         self::instance()->register_cpt();
         flush_rewrite_rules();
     }
 
-    public static function deactivate() {
+    public static function deactivate()
+    {
         flush_rewrite_rules();
     }
 
-    public function register_cpt() {
+    public function register_cpt()
+    {
         $labels = array(
-            'name'               => __( 'Publicações', 'publicacoes-submissao' ),
-            'singular_name'      => __( 'Publicação', 'publicacoes-submissao' ),
-            'menu_name'          => __( 'Publicações', 'publicacoes-submissao' ),
-            'name_admin_bar'     => __( 'Publicação', 'publicacoes-submissao' ),
-            'add_new'            => __( 'Adicionar Nova', 'publicacoes-submissao' ),
-            'add_new_item'       => __( 'Adicionar nova publicação', 'publicacoes-submissao' ),
-            'new_item'           => __( 'Nova publicação', 'publicacoes-submissao' ),
-            'edit_item'          => __( 'Editar publicação', 'publicacoes-submissao' ),
-            'view_item'          => __( 'Ver publicação', 'publicacoes-submissao' ),
-            'all_items'          => __( 'Todas as publicações', 'publicacoes-submissao' ),
-            'search_items'       => __( 'Buscar publicações', 'publicacoes-submissao' ),
-            'not_found'          => __( 'Nenhuma publicação encontrada.', 'publicacoes-submissao' ),
-            'not_found_in_trash' => __( 'Nenhuma publicação na lixeira.', 'publicacoes-submissao' ),
+            'name'               => __('Publicações', 'publicacoes-submissao'),
+            'singular_name'      => __('Publicação', 'publicacoes-submissao'),
+            'menu_name'          => __('Dia das Mães', 'publicacoes-submissao'),
+            'name_admin_bar'     => __('Dia das Mães', 'publicacoes-submissao'),
+            'add_new'            => __('Adicionar Nova', 'publicacoes-submissao'),
+            'add_new_item'       => __('Novo depoimento', 'publicacoes-submissao'),
+            'new_item'           => __('Nova publicação', 'publicacoes-submissao'),
+            'edit_item'          => __('Editar publicação', 'publicacoes-submissao'),
+            'view_item'          => __('Ver publicação', 'publicacoes-submissao'),
+            'all_items'          => __('Depoimentos', 'publicacoes-submissao'),
+            'search_items'       => __('Buscar publicações', 'publicacoes-submissao'),
+            'not_found'          => __('Nenhum publicação encontrada.', 'publicacoes-submissao'),
+            'not_found_in_trash' => __('Nenhuma publicação na lixeira.', 'publicacoes-submissao'),
         );
 
         $args = array(
@@ -67,10 +79,10 @@ class Publicacoes_Submissao {
             'taxonomies'         => array(),
             'menu_icon'          => 'dashicons-heart',
             'capability_type'    => 'post',
-            'supports'           => array( 'title', 'editor', 'thumbnail', 'comments' ),
+            'supports'           => array('title', 'editor', 'thumbnail', 'comments'),
             'show_in_rest'       => false,
         );
 
-        register_post_type( 'publicacoes', $args );
+        register_post_type('publicacoes', $args);
     }
 }
